d57acc8bebd3344eb9cb820ed6d87d6ba80aa80a
SzymonSzmydt <szymonszmydt@gmail.com>
2023-01-30 15:35:25 +0100

    add button with route

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const handleClick = () => {
+    console.log(" Placing your order");
+    router.push("/product");
+  };
   return (
     <div>
       <h1> Home Page </h1>
       <Link href="/blog">Blog</Link>
       <Link href="/product">Products</Link>
+      <button onClick={handleClick}>Place Order</button>
     </div>
   );
 }
