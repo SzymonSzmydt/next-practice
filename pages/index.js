fbcf7edee950fb958e6e5902260ff6ecbc6b6d79
SzymonSzmydt <szymonszmydt@gmail.com>
2023-02-02 13:10:02 +0100

    update data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <h1> Next js pre-rendering</h1>;
+  return (
+    <>
+      <h1> Next js pre-rendering</h1>
+      <Link href="/users"> users </Link>
+      <Link href="/posts"> Posts </Link>
+    </>
+  );
 }
---
+++ pages/posts/[postId].js
@@ -0,0 +1,50 @@
+function Post({ post }) {
+  return (
+    <>
+      <h2>
+        {post.id} {post.title}
+      </h2>
+      <p> {post.body}</p>
+    </>
+  );
+}
+
+export default Post;
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: { postId: "1" },
+      },
+      {
+        params: { postId: "2" },
+      },
+      {
+        params: { postId: "3" },
+      },
+    ],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(
+    `http://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = await response.json();
+
+  if (!data.id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  console.log(`Generate page for /posts/${params.postId}`);
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
