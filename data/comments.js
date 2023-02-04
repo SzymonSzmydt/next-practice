67c53324542e60302bb9091ec36500725e84423c
SzymonSzmydt <szymonszmydt@gmail.com>
2023-02-04 15:48:22 +0100

    creating data folder with comments

---
+++ data/comments.js
@@ -0,0 +1,14 @@
+export const comments = [
+  {
+    id: 1,
+    text: "This is the first text",
+  },
+  {
+    id: 2,
+    text: "This is the second text",
+  },
+  {
+    id: 3,
+    text: "This is the third text",
+  },
+];
